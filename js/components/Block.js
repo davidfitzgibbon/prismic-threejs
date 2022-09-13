a6c3f177e7ca61cf120f2c5bfeae93be137e5cc0
David Fitzgibbon <davidfitzgibbon@gmail.com>
2022-09-13 13:59:46 +0100

    Update Block.js

--- js/components/Block.js
+++ js/components/Block.js
@@ -8,25 +8,36 @@ export default class Block {
     const depth = sketch.blockWidth * 0.5
 
     const x = settings.x
-    const y = 20
+    const y = sketch.blockWidth
     const z = settings.z
 
     this.material = new THREE.MeshStandardMaterial({ color: 0xff0000 })
     this.geometry = new THREE.BoxGeometry(width, height, depth)
     this.mesh = new THREE.Mesh(this.geometry, this.material)
+
+    this.mesh.position.set(x, y, z)
+    this.mesh.lookAt(0, y, 0)
+
     sketch.scene.add(this.mesh)
 
     this.body = sketch.world.add({
       type: 'box',
       size: [width, height, depth],
       pos: [x, y, z],
-      rot: [0, 0, 0],
+      rot: [
+        this.radToDeg(this.mesh.rotation.x),
+        this.radToDeg(this.mesh.rotation.y),
+        this.radToDeg(this.mesh.rotation.z),
+      ],
       move: true,
       friction: 1,
       restitution: 0.1,
     })
 
     return this
+  }
+  radToDeg(num) {
+    return (num * 180) / Math.PI
   }
   update() {
     this.mesh.position.copy(this.body.getPosition())
